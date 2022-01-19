0a9e10b7087b4085978a42a55a2f250255ef70e8
Harry Shah <harvinder.shah@gmail.com>
2022-01-18 19:08:39 -0500

    tablr created and seeded

---
+++ server/db/CreateThoughtsTable.js
@@ -0,0 +1,36 @@
+const AWS = require('aws-sdk');
+
+// modify the AWS config object that DynamoDB will use to connect to the local instance
+AWS.config.update({
+    region: "us-east-2",
+    endpoint: "http://localhost:8000"
+  });
+
+// create the DynamoDB service object
+// we are using DynamoDB class to create a service interface object, dynamodb.
+const dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
+// create a params object that will hold the schema and metadata of the table
+const params = {
+    TableName: "Thoughts",
+    KeySchema: [
+        {AttributeName: "username", KeyType: "HASH"}, // Partition Key
+        {AttributeName: "createdAt", KeyType: "RANGE"} // Sort Key
+    ],
+    AttributeDefinitions: [
+        {AttributeName: "username", AttributeType: "S"},
+        {AttributeName: "createdAt", AttributeType: "N"}
+    ],
+    ProvisionedThroughput: {
+        ReadCapacityUnits: 10,
+        WriteCapacityUnits: 10
+    }
+};
+// Make a call to the DynamoDB instance to create the table
+dynamodb.createTable(params, (err, data) =>{
+    if (err) {
+        console.log("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+    }
+});
---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+const userRoutes = require('./routes/user-routes');
+
+// express middleware, used to be body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// serve up static assets
+if (process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+}
+
+// app.use(require('./routes));
+app.use('./api', userRoutes);
+
+// start the api server
+app.listen(PORT, () =>console.log(`API Server now listening on ${PORT}`));
